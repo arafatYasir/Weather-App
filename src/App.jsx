ee492c8b14be1f00ebf646de40e237a977889db8
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-07-30 04:34:53 +0600

    Real time added with dayjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CountrySelector from './components/CountrySelector';
+import dayjs from 'dayjs';
 
 const App = () => {
   const [country, setCountry] = useState("Bangladesh");
   const [city, setCity] = useState("Dhaka");
+  const [time, setTime] = useState(dayjs().format("HH:mm:ss"));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(dayjs().format("HH:mm:ss"));
+    }, 1000);
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <div className='min-h-screen bg-cover bg-center bg-no-repeat px-6 py-8' style={{ backgroundImage: "url(https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?q=80&w=974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)" }}>
 
@@ -11,6 +22,9 @@ const App = () => {
         <p className='text-xl font-bold mb-4'>
           {country} - {city}
         </p>
+
+        <p className='text-sm mb-4'>{time}</p>
+
         <CountrySelector
           country={country}
           setCountry={setCountry}
